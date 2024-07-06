12f157120997691be449a2cd7a12b5318d59dca3
Sara Peel <sarampeel@gmail.com>
2024-07-05 20:24:58 -0600

    Added rough home page formatting and some preliminary data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,31 +5,36 @@ import {
   Box
 } from "@mui/material";
 
-const Header = () => {
-  const buttonColor = "#000000";
+export const blue = "#3C4E73";
+export const darkbrown = "#40240C";
+export const orange = "#BF6F41";
+export const midbrown = "#73503C";
+export const lightbrown = "#D9A78B";
+export const offwhite = "#FAF7FC";
 
+const Header = () => {
   return (
     <AppBar position="static" 
       sx={{
-        backgroundColor: '#ffffff',
+        backgroundColor: offwhite,
         boxShadow: 'none',
       }}
     >
-      <Toolbar>
-        <Button href="/sara_peel" style={{ color: buttonColor }}>
+      <Toolbar style={{ maxWidth: "1290px" }}>
+        <Button href="/sara_peel" style={{ color: darkbrown }}>
           Sara Peel
         </Button>
         <Box sx={{ flexGrow: 1 }} />
-        <Button href="/sara_peel/about" style={{ color: buttonColor }}>
+        <Button href="/sara_peel/about" style={{ color: darkbrown }}>
           About
         </Button>
-        <Button href="/sara_peel/experience" style={{ color: buttonColor }}>
+        <Button href="/sara_peel/experience" style={{ color: darkbrown }}>
           Experience
         </Button>
-        <Button href="/sara_peel/projects" style={{ color: buttonColor }}>
+        <Button href="/sara_peel/projects" style={{ color: darkbrown }}>
           Projects
         </Button>
-        <Button href="/sara_peel/contact" style={{ color: buttonColor }}>
+        <Button href="/sara_peel/contact" style={{ color: darkbrown }}>
           Contact
         </Button>
       </Toolbar>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,31 @@
+import { 
+  Box,
+  Typography
+} from "@mui/material";
+import homeImage from "../../images/stgeorge-91.jpg";
+import { 
+  blue,
+  offwhite,
+} from "../Header.jsx"
+
 const Home = () => {
   return (
-    <div>
-      <p>Home Page</p>
-    </div>
+    <Box style={{ 
+      width: "100%", 
+      height: "100%",
+      display: "flex",
+      flexDirection: "row",
+      }} 
+    >
+      <Box style={{ flexGrow: 1, backgroundColor: blue }}>
+        <Typography style={{ color: offwhite }}>
+          Developer / Designer / Doesn't like the Dentist
+        </Typography>
+      </Box>
+      <Box style={{ flexGrow: 1, backgroundColor: offwhite }} >
+        <img src={homeImage} alt="Sara" style={{ objectFit: "contain", height: "98vh", width: "auto" }} />
+      </Box>
+    </Box>
   );
 };
 
